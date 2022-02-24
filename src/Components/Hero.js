6508f4f9a3096137f5027d3aea41292836d70c7b
Stephanie Mitchell <stephsorandom@gmail.com>
2022-02-23 21:49:05 -0500

    navbar and hero section

---
+++ src/Components/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <section className="hero">
+      <img src="./images/hero.png" className="hero-pic" />
+      <h1>Online Experiences</h1>
+      <span>
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </span>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <img src="./images/airbnbLogo.png" className='nav-logo' />
+    </nav>
+  );
+}
+
+export default Navbar;
