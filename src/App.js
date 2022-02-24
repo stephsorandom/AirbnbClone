886ec30572a17a3f34ab03c58dd4952b6809e93f
Stephanie Mitchell <stephsorandom@gmail.com>
2022-02-23 22:21:09 -0500

    components finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
+import Card from "./Components/Card";
+import data from "./data";
 
 function App() {
+  const cards = data.map(item => {
+    return (
+      <Card 
+        key={item.id} 
+        item={item} 
+      />
+    )
+  });
+
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <section className='cards-list' >
+          {cards}
+      </section>
     </div>
   );
 }
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -4,8 +4,8 @@ function Hero() {
   return (
     <section className="hero">
       <img src="./images/hero.png" className="hero-pic" />
-      <h1>Online Experiences</h1>
-      <span>
+      <h1 className='hero-header'>Online Experiences</h1>
+      <span className='hero-text'>
         Join unique interactive activities led by one-of-a-kind hosts—all
         without leaving home.
       </span>
